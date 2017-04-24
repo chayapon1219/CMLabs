7c83e5c7f6c2865a30da0f20c9e2de7280baf4cd
Gnop <konota_ism@hotmail.com>
2017-04-24 18:34:21 +0700

    [B] commit at 18

--- bloodgroup.c
+++ bloodgroup.c
@@ -5,11 +5,11 @@ void main()
   int i;
   do
   {
-    printf("What is your Name? ");
+    printf("Please Enter your name and sir name !!");
     scanf("%s", Name);
     do
     {
-      printf("What is your blood group [A, B, AB, or O]? ");
+      printf("What type of your blood group??");
       scanf("%s", G);
 
       if (strcmp(G, "A") !=0  &&
